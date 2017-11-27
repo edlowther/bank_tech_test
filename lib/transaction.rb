34172dff1195f00255e0d317b75528b7fb238e62
Ed Lowther <ed.lowther[at]gmail.com>
2017-11-27 13:20:47 +0000

    Categorise transactions on initialization as deposits or withdrawals

--- lib/transaction.rb
+++ lib/transaction.rb
@@ -1,8 +1,9 @@
 class Transaction
-  attr_reader :amount, :date, :account_balance
-  def initialize(amount, date, account_balance)
+  attr_reader :amount, :date, :account_balance, :type
+  def initialize(amount, date, account_balance, type)
     @amount = amount
     @date = date
     @account_balance = account_balance
+    @type = type
   end
 end
--- spec/units/transaction_spec.rb
+++ spec/units/transaction_spec.rb
@@ -3,7 +3,7 @@ require 'transaction'
 describe Transaction do
   date = Date.new(2017,11,27)
   let(:account_balance) { 2500 }
-  subject(:transaction) { described_class.new(2000, date, account_balance) }
+  subject(:transaction) { described_class.new(2000, date, account_balance, :deposit) }
 
   it 'has an amount' do
     expect(transaction.amount).to eq(2000)
@@ -15,5 +15,15 @@ describe Transaction do
 
   it 'contains the new account balance as a result of the transaction' do
     expect(transaction.account_balance).to eq(account_balance)
+  end
+
+  it 'knows if it is a deposit' do
+    deposit = described_class.new(50, date, account_balance, :deposit)
+    expect(deposit.type).to eq(:deposit)
+  end
+
+  it 'knows if it is a withdrawal' do
+    deposit = described_class.new(50, date, account_balance, :withdrawal)
+    expect(deposit.type).to eq(:withdrawal)
   end
 end
