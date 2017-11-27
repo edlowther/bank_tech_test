27b17efca14e030ce33a35e97c501d4991ce01e1
Ed Lowther <ed.lowther[at]gmail.com>
2017-11-27 12:54:21 +0000

    Add account balance to Transaction class

--- lib/transaction.rb
+++ lib/transaction.rb
@@ -1,7 +1,8 @@
 class Transaction
-  attr_reader :amount, :date
-  def initialize(amount, date)
+  attr_reader :amount, :date, :account_balance
+  def initialize(amount, date, account_balance)
     @amount = amount
     @date = date
+    @account_balance = account_balance
   end
 end
--- spec/units/transaction_spec.rb
+++ spec/units/transaction_spec.rb
@@ -2,13 +2,18 @@ require 'transaction'
 
 describe Transaction do
   date = Date.new(2017,11,27)
-  subject(:transaction) { described_class.new(2000, date) }
-  
+  let(:account_balance) { 2500 }
+  subject(:transaction) { described_class.new(2000, date, account_balance) }
+
   it 'has an amount' do
     expect(transaction.amount).to eq(2000)
   end
 
   it 'has a date' do
     expect(transaction.date).to eq(date)
+  end
+
+  it 'contains the new account balance as a result of the transaction' do
+    expect(transaction.account_balance).to eq(account_balance)
   end
 end
